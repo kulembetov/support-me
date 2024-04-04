4aff75e528ad1a7d58607536b351eca75ede859f
Projects <artur_kulembetov@outlook.com>
2024-04-04 22:13:39 +0700

    feat: landing page

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+import './globals.css';
+import { cn } from '@/lib/utils';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+export const metadata: Metadata = {
+  title: 'SupportMe',
+  description: 'shadcn-ui + TailwindCSS + NextJS',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn(poppins.className, 'dark')}>{children}</body>
+    </html>
+  );
+}
